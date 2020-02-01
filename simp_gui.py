0e390e53e257692dbf0de727cbda8e270f14c918
Demdillypickles <46602704+Demdillypickles@users.noreply.github.com>
2020-01-31 18:53:15 -0600

    Update simp_gui.py

--- simp_gui.py
+++ simp_gui.py
@@ -5,3 +5,4 @@ while True:
     query = Sg.PopupGetText('SpellGen', 'Enter spell name to retrieve entry')
     text = Spell(query).fulltext
     Sg.PopupScrolled(text, title=query)
+    # Press OK to continue with loop, bringing the search query up again
