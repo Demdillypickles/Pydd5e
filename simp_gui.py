78e947963523e79c310d4bd48bc99244d4b1ce16
Demdillypickles <46602704+Demdillypickles@users.noreply.github.com>
2020-02-14 19:01:04 -0600

    Delete simp_gui.py

--- simp_gui.py
+++
@@ -1,8 +0,0 @@
-import PySimpleGUI as Sg
-from SpellGen import Spell
-
-while True:
-    query = Sg.PopupGetText('SpellGen', 'Enter spell name to retrieve entry')
-    text = Spell(query).fulltext
-    Sg.PopupScrolled(text, title=query)
-    # Press OK to continue with loop, bringing the search query up again
